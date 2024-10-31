d240183e8f39e3b11cb534ec55d9022c6a6ee26a
Alejandro Parrado <alejopadido13@gmail.com>
2024-10-31 10:48:02 -0500

    Puse un comentario

--- src/main/java/com/example/persointegrado/HelloApplication.java
+++ src/main/java/com/example/persointegrado/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+// Este es un cambio
     public static void main(String[] args) {
         launch();
     }
